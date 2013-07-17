4de80324f6dc85ed9d7ca262e9b01c4237deb2f5
ecoad <elliotcoad@gmail.com>
2013-07-17 23:08:17 +0100

    Adding Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    devFiles: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js'],
+    jshint: {
+      src: ['<%= devFiles %>'],
+      options: {
+        laxcomma: true,
+        globals: {
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= devFiles %>'],
+      tasks: ['jshint', 'mochacli']
+    },
+    mochacli: {
+        options: {
+            require: ['should'],
+            files: 'test/*-test.js'
+        },
+        spec: {
+            options: {
+                reporter: 'spec'
+            }
+        }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-mocha-cli');
+
+  grunt.registerTask('default', ['jshint', 'mochacli']);
+};
